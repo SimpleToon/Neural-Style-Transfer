2b539913a78f8b878ff68f7bd10ba380abab506a
YiQuan <yiquanneo@outlook.sg>
2026-02-01 20:06:10 +0800

    Updated tooltip text

--- Website/frontend/component/tooltip.js
+++ Website/frontend/component/tooltip.js
@@ -14,7 +14,7 @@ function ToolTip({ text }) {
             >
                 <img src="info.svg" alt="More info" className="w-4 h-4" />
             </button>
-            {open && <aside className={`absolute left-4 top-2 z-3 border-2 border border-zinc-700 bg-zinc-900 text-white text-xs px-2 py-1.5 shadow-lg max-w-[100vw] min-w-30 md:min-w-40 font-bold sm:left-4 max-sm:right-4 max-sm:left-auto`}>
+            {open && <aside className={`absolute md:left-4 md:top-2 z-3 border-2 border border-zinc-700 bg-zinc-900 text-white text-xs px-2 py-1.5 shadow-lg min-w-30 md:min-w-40 font-bold right-4 left-auto`}>
                 {text}
             </aside>}
         </div>
